5e86fa559a09d15643358518488db695a06cd87e
Lewin Chan <lewin.chan@adaptris.com>
2016-11-23 09:55:37 +0000

    INTERLOK-694 Added the build script.

---
+++ create-docs-site.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+TARGET_DIR=/home/websites/development.adaptris.net/docs/
+SITE_DIR=_site
+SITE_NAME=Interlok
+
+rm -rf ./${SITE_DIR}/${SITE_NAME}
+rvm use 2.3.0
+bundle exec jekyll build -d ${SITE_DIR}/${SITE_NAME}
+rc=`echo $?`
+if [ $rc -ne 0 ]
+then
+ echo Jekyll Failed
+ exit $rc
+else
+ echo Deploying ${SITE_NAME} to ${TARGET_DIR}
+ rm -rf ${TARGET_DIR}/${SITE_NAME}
+ mv ${SITE_DIR}/${SITE_NAME} ${TARGET_DIR}
+fi
+
+
