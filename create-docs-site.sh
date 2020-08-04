95b3910dbb8235ad176e5a7061ab47bb64670cb2
Lewin Chan <lewin.chan@adaptris.com>
2020-08-04 11:54:24 +0100

    INTERLOK-3323 Prep for storing the artifact in jenkins

--- create-docs-site.sh
+++ create-docs-site.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-TARGET_DIR=/home/websites/development.adaptris.net/docs/
 SITE_DIR=_site
-SITE_NAME=Interlok
 
 source ${HOME}/.rvm/scripts/rvm
-
-rm -rf ./${SITE_DIR}/${SITE_NAME}
-rvm use 2.3.3
-bundle exec jekyll build -d ${SITE_DIR}/${SITE_NAME}
-rc=`echo $?`
-if [ $rc -ne 0 ]
-then
+rm -rf ./${SITE_DIR} html-docs.zip
+rvm use 2.7
+gem install bundler
+bundle install
+bundle exec jekyll build -d ${SITE_DIR}
+if [[ $? -ne 0 ]]; then
  echo Jekyll Failed
- exit $rc
-else
- echo Deploying ${SITE_NAME} to ${TARGET_DIR}
- rm -rf ${TARGET_DIR}/${SITE_NAME}
- mv ${SITE_DIR}/${SITE_NAME} ${TARGET_DIR}
+ exit 1
 fi
-
-
+(cd ./${SITE_DIR} && zip -9 -r ../html-docs.zip *)
